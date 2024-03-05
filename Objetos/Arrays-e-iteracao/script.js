591ea2407ded93a3f04fe1def9c6b0017749cbe4
Emanoel Pontes Timbo <emanoelpontes8@gmail.com>
2024-03-05 20:23:58 -0300

    Array e Iteração parte 2

--- Objetos/Arrays-e-iteracao/script.js
+++ Objetos/Arrays-e-iteracao/script.js
@@ -67,23 +67,87 @@
 // console.log(arrayNomeAulas);
 
 //Reduce() outra forma de iterar
-const aulas = [10, 25, 30];
+// const aulas = [10, 25, 30];
 
-const reduceAulas = aulas.reduce((acumulador, item) => {
-  console.log(acumulador, item);
-  return acumulador + item;
-}, 0); //--> comaça com zero, más poderia começar com qualquer outro valor e se não colocar nenhum valor ele pula a primira iteracao
+// const reduceAulas = aulas.reduce((acumulador, item) => {
+//   console.log(acumulador, item);
+//   return acumulador + item;
+// }, 0); //--> comaça com zero, más poderia começar com qualquer outro valor e se não colocar nenhum valor ele pula a primira iteracao
 
-console.log(reduceAulas); //65
+// console.log(reduceAulas); //65
 
-const numeros = [10, 25, 30, 3, 54, 33, 22];
+// const numeros = [10, 25, 30, 3, 54, 33, 22];
 
-const maiorNumero = numeros.reduce((anterior, atual) => {
-  if (anterior > atual) {
-    return anterior;
-  } else {
-    return atual;
-  }
+// const maiorNumero = numeros.reduce((anterior, atual) => {
+//   if (anterior > atual) {
+//     return anterior;
+//   } else {
+//     return atual;
+//   }
+// });
+
+// console.log(maiorNumero);
+
+const aulas = [
+  {
+    nome: "HTML 1",
+    min: 15,
+  },
+  {
+    nome: "HTML 2",
+    min: 10,
+  },
+  {
+    nome: "CSS 1",
+    min: 20,
+  },
+  {
+    nome: "JS 1",
+    min: 25,
+  },
+];
+
+const listaAulas = aulas.reduce((acumulador, aula, index) => {
+  // console.log(index);
+  acumulador[index] = aula.nome;
+  return acumulador;
+}, {}); //o acumulador começa com obj vazio {}  //Aula guarda o objeto //Index guarda o index em si
+
+//reduceRight() começa da direita para esquerda
+
+const frutas = ["Banana", "Pêra", "Uva"];
+
+const frutasRight = frutas.reduceRight((acc, fruta) => acc + " " + fruta);
+const frutasLeft = frutas.reduce((acc, fruta) => acc + " " + fruta);
+
+console.log(frutasRight); // Uva Pêra Banana
+console.log(frutasLeft); // Banana Pêra Uva
+
+//some() se pelo menos um return da iteração for true, ele retorna true: é como se fosse o 'OU'
+
+const temUva = frutas.some((fruta) => {
+  return fruta === "Uva";
 });
 
-console.log(maiorNumero);
+console.log(temUva); //true
+
+function maior100(numero) {
+  return numero > 100;
+}
+
+const numeros = [10, 14, 43, 88, 101, 2];
+const temMaior = numeros.some(maior100); //'se' chama a função maior100, tendo numero maior ele retorna true
+console.log(temMaior);
+
+//every() verifica se todas as intereções retornam true se pelo menos uma for false ele retorna false
+const frutas1 = ["Banana", "Pêra", "Uva", ""];
+const arrayCheia = frutas1.every((fruta) => {
+  return fruta;
+});
+console.log(arrayCheia); //flase pq na array existe um item vazio
+
+const num = [10, 14, 43, 33, 102, 9];
+const maior3 = num.every((n) => {
+  return n > 8;
+});
+console.log(maior3);
