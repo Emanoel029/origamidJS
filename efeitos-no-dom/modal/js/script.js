5a89ae87a5809ca1977450b134d24a218bf1fe7c
Emanoel Pontes Timbo <emanoelpontes8@gmail.com>
2024-03-14 18:10:16 -0300

    Atualizações do curso JS origamid

--- efeitos-no-dom/modal/js/script.js
+++ efeitos-no-dom/modal/js/script.js
@@ -2,8 +2,12 @@ import scroollSuave from "./modules/scroll-suave.js";
 import initAnimacaoScroll from "./modules/scroll-animacao.js";
 import initAccordion from "./modules/accordion.js";
 import initTabNav from "./modules/tabnav.js";
+import initModal from "./modules/modal.js";
+import iniTooltip from "./modules/tooltip.js";
 
 scroollSuave(); // animação do scroll
 initAnimacaoScroll(); // animação de quando vai dando o scroll vai aparecendo
 initAccordion(); // animação das FAQ
 initTabNav(); //animaçõa da descrição das fotos
+initModal();
+iniTooltip();
